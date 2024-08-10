c7d24f954c04a369332b888f8dc9838f1fa3ccc0
Julio Cesar Martinez Ramirez <57429237+jcontrolresearch@users.noreply.github.com>
2024-08-09 22:15:44 -0500

    Create If_else.py

---
+++ Ejercicios_Ejemplos/If_else.py
@@ -0,0 +1,60 @@
+# SI?
+# if --- en espanol es si?
+# SI NO?
+# if else --- en espanol si no?
+# NO?
+# else --- en espanol represnta el no?
+# los bloques de codigo de accion se representa con dos puntos :
+
+
+matematicas_calificacion = 6
+
+# si la calificacion es mayor que 6 entones  aprobo
+
+# Siiiiiiiiiiiiiiiii!
+if matematicas_calificacion >= 6 :
+    print("Felicidades aprobaste")
+    if matematicas_calificacion >= 8.5:
+        print("Puedes estar en el cuadro de honor! Sigue asi!")
+# NOOoooooooooooooooo
+else:
+    print("Felicidades repbaste")
+
+# # Siiiiiiiiiiiiiiiii!
+# if matematicas_calificacion >= 6 and matematicas_calificacion <8.5 :
+#     print("Aprobaste, no hay nada que festejar")
+# elif matematicas_calificacion >= 8.5:
+#     print("Puedes estar en el cuadro de honor! Sigue asi!")
+# # NOOoooooooooooooooo
+# else:
+#     print("NIMODO")matematicas_calificacion
+
+
+# CONTROL DE FLUJO: BUCLE
+
+# Hay tres tipos de bucle:
+# while -- mientras se cumpla una condicion
+# do while -- al menos se ejecuta una condicion
+# for --- iterar secuencia
+
+# Ejemplo del For = ITERAR
+# FOR - PARA
+# WHILE - MIENTRAS
+# DO WHILE - HACER MIENTRAS
+
+
+# Me mandan por las compra y me piden traer:
+# 1. 1kg de manzanas
+# 2. 1/2 kilo de platano
+# 3. 1 agucate
+# ....
+
+# Para un elemento en mi liosta de conpras:
+
+# for item in shopping_list:
+#     item
+
+# WHILE - mientras
+
+# while escucha?:
+#     obtener lo que le estamos diciendo
